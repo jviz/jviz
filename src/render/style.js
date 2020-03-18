8e0365ce312438d6cd87cb748a379870c9195145
Josemi <josemijuanes@gmail.com>
2020-03-18 14:16:59 +0100

    Minor fixes in style manager

--- src/render/style.js
+++ src/render/style.js
@@ -1,4 +1,4 @@
-import {isArray, each} from "../util.js";
+//import {isArray, each} from "../util.js";
 
 //Style props
 export const styleProps = {
@@ -6,44 +6,59 @@ export const styleProps = {
     "fill": "fill",
     //Stroke props
     "stroke": "stroke",
-    "strokeWidth": "stroke-width",
-    "strokeDash": "stroke-dasharray",
-    "strokeDashOffset": "stroke-dashoffset",
-    "strokeLineCap": "stroke-linecap",  //Values: butt|square|round
-    "strokeLineJoin": "stroke-linejoin",  //Values: arcs|bevel|miter|miter-clip|round
+    "strokewidth": "stroke-width",
+    "strokedash": "stroke-dasharray",
+    "strokedashoffset": "stroke-dashoffset",
+    "strokelinecap": "stroke-linecap",  //Values: butt|square|round
+    "strokelinejoin": "stroke-linejoin",  //Values: arcs|bevel|miter|miter-clip|round
     //Text
     "color": "color",
-    "fontSize": "font-size",
-    "fontFamily": "font-family",  //Values: arial|Times|serif
-    "fontStyle": "font-style",  //Values: normal|italic|oblique
-    "fontWeight": "font-weight",  //Values: normal|bold
+    "fontsize": "font-size",
+    "fontfamily": "font-family",  //Values: arial|Times|serif
+    "fontstyle": "font-style",  //Values: normal|italic|oblique
+    "fontweight": "font-weight",  //Values: normal|bold
     //Border
     "border": "border",
-    "borderColor": "border-color",
-    "borderStyle": "border-style",
-    "borderWidth": "border-width",
+    "bordercolor": "border-color",
+    "borderstyle": "border-style",
+    "borderwidth": "border-width",
     //General
     "opacity": "opacity",
     "cursor": "cursor",
     "transform": "transform"
 };
 
-//Apply shape stypes
-export function applyStyle (context, datum, props, target) {
-    //Draw all available styles
-    return each(props, function (key, value) {
-        //Check if this key is a style prop
-        if (typeof styleProps[key] !== "undefined" && value !== null) {
-            let parsedValue = context.value(props[key], ((isArray(datum) === true) ? datum[0] : datum)); //Get value from context
-            //Add this css style
-            target.style(styleProps[key], parsedValue);
-        }
-    });
+//Apply style
+//export function applyStyle (context, datum, props, target) {
+//    //Draw all available styles
+//    return each(props, function (key, value) {
+//        //Check if this key is a style prop
+//        if (typeof styleProps[key] !== "undefined" && value !== null) {
+//            let parsedValue = context.value(props[key], ((isArray(datum) === true) ? datum[0] : datum)); //Get value from context
+//            //Add this css style
+//            target.style(styleProps[key], parsedValue);
+//        }
+//    });
+//}
+
+//Set style property to element
+export function setStyle (element, name, value) {
+    return element.style(styleProps[parseStyleName(name)], value);
+}
+
+//Parse style name
+export function parseStypeProp (name) {
+    return name.replace(/-/g, "").toLowerCase();
+}
+
+//Check if the provided name is a valid style property
+export function isStyleName (name) {
+    return typeof name === "string" && typeof styleProps[parseStyleName(name)] !== "undefined";
 }
 
 //Clear all styles of an element
 export function cleanStyle (target) {
-    target.each(function () {
+    return target.each(function () {
         this.style.cssText = "";
     });
 }
