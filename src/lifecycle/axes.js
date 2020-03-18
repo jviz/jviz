fdd8300e84092565486d72fae1fe725239debb3b
Josemi <josemijuanes@gmail.com>
2020-03-18 17:54:50 +0100

    Fixed imports in axes manager

--- src/lifecycle/axes.js
+++ src/lifecycle/axes.js
@@ -1,5 +1,5 @@
 import {colors} from "../color.js";
-import {polyline} from "../shapes/paths/polyline.js";
+import {polyline} from "../render/primitives/polyline.js";
 import {isArray} from "../util.js";
 import {isContinuousScale, isDiscreteScale} from "../scales/index.js";
 import {getExpressionSources} from "../runtime/expression.js";
