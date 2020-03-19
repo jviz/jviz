82e9a7f2c87d89ffa94673267f5db6d025d117bf
Josemi <josemijuanes@gmail.com>
2020-03-19 18:06:09 +0100

    Updated public api: added schema parser and implemented new options

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import {colors} from "./color.js";
-import {compile} from "./compiler.js";
+import {parseSchema} from "./schema.js";
 import {each} from "./util.js";
 import {evaluate} from "./evaluate.js";
-import {select, selectAll} from "./select/index.js";
+import {select, selectAll} from "./render/selection.js";
 import {createContext} from "./context.js";
+
+//Get or set context values
+let getOrSetContextValue = function (self, node, name, value) {
+    //Check if node is not defined
+    if (typeof node === "undefined" || node === null) {
+        throw new Error(`Unknown type "${name}"`); //TODO: improve error
+    }
+    //Check for no value provided --> return the current node value
+    if (typeof value === "undefined") {
+        return node.value;
+    }
+    //Update the data input
+    return self.context.addAction(node, value);
+};
+
+//Default options
+let defaultOptions = {
+    "parent": null
+};
 
 //Main Jviz instance
 let jviz = function (schema, options) {
@@ -12,15 +31,14 @@ let jviz = function (schema, options) {
         return new jviz(schema, options);
     }
     //TODO: validate options object
-    this.options = options;
-    this.parent = select(options.parent); //Select parent element
-    //this.parent.attr("width", props.width);
-    //this.parent.svg.attr("height", props.height);
-    //let spec = this.options.spec; //TODO: parse specification object
+    //this.options = options;
+    options = Object.assing({}, defaultOptions, options);
     //Create the new viewer context
-    Object.assign(this, {
-        "context": createContext(schema, this.parent)
-    });
+    this.context = createContext(schema, options);
+    //Check for parent component provided
+    if (typeof options.parent !== "undefined" && options.parent !== null) {
+        this.context.scene.mount(parent); //Mount scene
+    }
 };
 
 //Initialize api methods
@@ -29,39 +47,25 @@ jviz.prototype = {
     "render": function (callback) {
         return this.context.render(callback);
     },
-    //Get or set context values
-    "_getOrSetContextValue": function (name, node, value) {
-        //Check if node is not defined
-        if (typeof node === "undefined") {
-            throw new Error(`Unknown type "${name}"`); //TODO: improve error
-        }
-        //Check for no value provided --> return the current node value
-        if (typeof value === "undefined") {
-            return node.value;
-        }
-        //Update the data input
-        this.context.createAction(node, value);
-        return this;
-    },
     //Get or set context state values
     "state": function (name, value) {
-        return this._getOrSetContextValue(name, this.context.state[name], value);
+        return getOrSetContextValue(this, this.context.state[name], name, value);
     },
     //Get or set the context data
     "data": function (name, value) {
-        return this._getOrSetContextValue(name, this.context.input[name], value);
+        return getOrSetContextValue(this, this.context.input[name], name, value);
     },
     //Get or set the plot width
     "width": function (value) {
-        return this._getOrSetContextValue("width", this.context.draw.width, value); 
+        return getOrSetContextValue(this, this.context.draw.width, "width", value); 
     },
     //Get or set the plot height
     "height": function (value) {
-        return this._getOrSetContextValue("height", this.context.draw.height, value);
+        return getOrSetContextValue(this, this.context.draw.height, "height", value);
     },
     //Get or set the plot padding
     "padding": function (value) {
-        return this._getOrSetContextValue("padding", this.context.draw.padding, value);
+        return getOrSetContextValue(this, this.context.draw.padding, "padding", value);
     },
     //Register event listener
     "addEventListener": function (name, listener) {
@@ -70,17 +74,22 @@ jviz.prototype = {
     //Remove event listener
     "removeEventListener": function (name, listener) {
         return this.context.events.removeEventListener(name, listener);
+    },
+    //Destroy the viewer --> unmount the scene and remove listeners
+    "destroy": function () {
+        this.context.scene.unmount(); //Unmount scene
+        //TODO: remove context listeners
     }
 };
 
 //Assign some utils
 Object.assign(jviz, {
-    colors,
-    compile,
-    each,
-    evaluate,
-    select,
-    selectAll
+    "colors": colors,
+    "each": each,
+    "evaluate": evaluate,
+    "schema": parseSchema,
+    "select": select,
+    "selectAll": selectAll
 });
 
 //Export jviz
