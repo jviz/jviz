260bf03c16b8ed6b82afdf2868b8980eba9922a1
Josemi <josemijuanes@gmail.com>
2020-03-23 11:42:52 +0100

    Init jviz loader for webpack

---
+++ loader.js
@@ -0,0 +1,15 @@
+let jviz = require("./jviz.cjs.js");
+
+//Stringify the chema
+let schemaStringify = function (schema) {
+    return JSON.stringify(schema).replace(/\u2028/g, "\\u2028").replace(/\u2029/g, "\\u2029");
+};
+
+//Export jviz schema loader
+module.exports = function (source) {
+    //Build schema
+    let schema = jviz.schemaSync(source);
+    //Return parsed schema
+    return `export default ${schemaStringify(schema)}`
+};
+
