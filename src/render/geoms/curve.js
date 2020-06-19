778142952ad03e41cad8400320c3c469119fa1a0
Josemi <josemijuanes@gmail.com>
2020-06-19 14:41:48 +0200

    render: renamed line geom as curve and moved to render/geoms folder

--- src/render/geoms/curve.js
+++ src/render/geoms/curve.js
@@ -1,41 +1,41 @@
-import {propTypes} from "../props.js";
-import {path} from "../render/path.js";
-import {getCurve} from "../render/curves/index.js";
+import {path} from "../path.js";
+import {getCurve} from "../curves/index.js";
+import {propTypes} from "../../props.js";
 
-//Line default props
+//Curve default props
 let defaultProps = {
     "x": propTypes.number(0),
     "y": propTypes.number(0),
     "curve": propTypes.string("linear") // linear|catmull-roll|step|step-after|step-before
 };
 
-//Generate a line (or polyline)
-function line (args) {
+//Generate a curve
+function curve (args) {
     let data = args.data;
-    let linePath = path();
+    let curvePath = path();
     //Get the wanted curve name 
     //--> if no curve is provided, linear will be used
-    let curve = getCurve(args.curve)(linePath); //initialize the curve
-    //Start drawing the polyline
+    let curveDraw = getCurve(args.curve)(curvePath); //initialize the curve
+    //Start drawing the curve
     for (let i = 0; i < data.length; i++) {
-        curve.point(args.x(data[i], i), args.y(data[i], i));
+        curveDraw.point(args.x(data[i], i), args.y(data[i], i));
     }
-    curve.end(); //Finish the curve
-    return linePath.toString();
+    curveDraw.end(); //Finish the curve
+    return curvePath.toString();
 }
 
-//Export lie shape
-export const lineShape = {
+//Export curve geom
+export const curveGeom = {
     "tag": "path",
-    "type": "line",
+    "type": "curve",
     "render": function (context, data, props, element) {
         element.attr("fill", "none"); //Hack to prevent filled polyline 
         return element.attr("d", line({
             "x": function (datum) {
-                return context.value(props.x, datum, 0);
+                return context.value(props.x, datum, defaultProps.x.defaultValue);
             },
             "y": function (datum) {
-                return context.value(props.y, datum, 0);
+                return context.value(props.y, datum, defaultProps.y.defaultValue);
             },
             "curve": context.value(props.curve, data[0], defaultProps.curve.defaultValue),
             "data": data
