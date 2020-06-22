783347a0f09b7bdeef11dc3140e6194d63ee1448
Josemi <josemijuanes@gmail.com>
2020-06-22 12:02:35 +0200

    render: redesigned scene manager --> now using context nodes and selection for manage the scene element

--- src/render/scene.js
+++ src/render/scene.js
@@ -1,61 +1,55 @@
+import {isObject} from "../util.js";
 import {createNode, selectNodes, removeNode} from "./util/nodes.js";
-import {Selection} from "./selection.js";
+import {select, Selection} from "./selection.js";
+import {setStyle} from "./style.scss";
 
-//Scene class
-export function Scene () {
-    this.node = createNode("svg", null); //Create the scene node
-    this.node.setAttribute("width", "0px"); //Initialize scene width
-    this.node.setAttribute("height", "0px"); //Initialize scene height
-    //this.parent = null; //Parent node
-}
+//Scene style parameters
+let sceneStyles = ["background", "fontFamily", "fontSize"];
 
-//Scene prototype
-Scene.prototype = {
-    //Set the scene style
-    "style": function (name, value) {
-        //TODO: check for a valid style value
-        return this.node.style.setProperty(name, value);
-    },
-    //Set the scene width
-    "width": function (value) {
-        return this.node.setAttribute("width", value);
-    },
-    //Set the scene height
-    "height": function (value) {
-        return this.node.setAttribute("height", value);
-    },
-    //Calculate the scene size
-    "size": function () {
-        return this.node.getBoundingClientRect();
-    },
-    //Mount the scene
-    "mount": function (query) {
-        let parent = selectNodes(query); //Select parent node
-        if (parent.nodes.length > 0) {
-            parent.nodes[0].appendChild(this.node); //Mount 
-        }
-    },
-    //Unmount the scene
-    "unmount": function () {
-        if (this.node.parentNode !== null) {
-            return removeNode(this.node); //Remove node
-        }
-    },
-    //Append a new element to the scene
-    "append": function (name) {
-        let element = createNode(name, this.node);
-        this.node.appendChild(element); //Append the new element
-        return new Selection([element], this.node); //Return a selection with the element
+//Mount an scene
+export function mountScene (context, query) {
+    let element = context.scene.element.nodes[0]; //Get scene node
+    let parent = selectNodes(query); //Select parent node
+    if (parent.nodes.length > 0) {
+        parent.nodes[0].appendChild(element); //Mount 
     }
-};
-
-//Create a new scene
-export function createScene () {
-    return new Scene();
 }
 
-//Check if the provided value is an instance of a scene
-export function isScene (value) {
-    return value instanceof Scene;
+//Unmount the scene
+export function unmountScene (context) {
+    let element = context.scene.element.nodes[0]; //Get scene node
+    if (element.parentNode !== null) {
+        return removeNode(element); //Remove node
+    }
 }
+
+//Create a scene node
+export function createSceneNode (context, index, props) {
+    let theme = context.theme; //Get theme reference
+    let node = context.addNode({
+        "id": "scene",
+        "type": "scene",
+        "element": select(createNode("svg", null)),
+        "value": (isObject(props)) ? props : {}
+    });
+    //Initialize scene attributes
+    node.element.attr("width", "0px").attr("height", "0px");
+    //Set scene style
+    //TODO: this should be moved to updateSceneNode when scene style updates where allowed
+    sceneStyles.forEach(function (name) {
+        setStyle(node.element, name, (typeof node.value[name] === "string") ? node.value[name] : theme[name]);
+    });
+    //Add this node as a dependency of the width and height nodes
+    context.draw.width.targets.add(node.id, node);
+    context.draw.height.targets.add(node.id, node);
+    //Save this node as the context scene
+    context.scene = node;
+}
+
+//Update a scene node
+export function updateSceneNode (context, node) {
+    node.element.attr("width", context.draw.width.value); //Update width
+    node.element.attr("height", context.draw.height.value); //Update height
+}
+
 
