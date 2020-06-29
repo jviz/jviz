b0e62cd6e44578a6e2745d654c2b57c7f6d69e74
Josemi <josemijuanes@gmail.com>
2020-06-29 14:04:31 +0200

    scales: added invert method for linear scale

--- src/scales/linear.js
+++ src/scales/linear.js
@@ -1,3 +1,5 @@
+import {clamp} from "../math.js";
+
 // Verify an array
 let verifyArray = function (array, length) {
     return typeof array === "object" && Array.isArray(array) && array.length === length;
@@ -6,11 +8,6 @@ let verifyArray = function (array, length) {
 //Parse a boolean value
 let parseBool = function (value) {
     return value === true || value === "true";
-};
-
-//Linear scale default props
-let linearDefaultProps = {
-    "zero": false //should include zero in the domain
 };
 
 // Build a linear scale
@@ -25,18 +22,20 @@ export function linear (args) {
     //Return the scale generator
     let scale = function (value) {
         //Get the value in the domain provided
-        let v = Math.min(domain[1], Math.max(domain[0], value)); 
+        let v = clamp(value, domain[0], domain[1]); 
         return args.range[0] + (args.range[1] - args.range[0]) * (v - domain[0]) / (domain[1] - domain[0]);
     };
     //Add scale metadata
     scale.type = "linear";
     scale.range = args.range;
     scale.domain = domain;
+    //Invert the scale transform
+    scale.invert = function (value) {
+        let v = clamp(value, args.range[0], args.range[1]);
+        return domain[0] + (v - args.range[0]) * (domain[1] - domain[0]) / (args.range[1] - args.range[0]);
+    };
     //Return the scale
     return scale;
 }
-
-//Add default props to linear scale
-linear.defaultProps = linearDefaultProps;
 
 
