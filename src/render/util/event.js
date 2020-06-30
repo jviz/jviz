073516a66edcc16a89080b1fa56f098cc8c44875
Josemi <josemijuanes@gmail.com>
2020-06-30 22:02:47 +0200

    render: fixed bug parsing events --> wrong scene element

--- src/render/util/event.js
+++ src/render/util/event.js
@@ -5,10 +5,13 @@ export const tooltipHideEvent = "mouseleave";
 
 //Parse event
 export function parseEvent (context, event) {
+    let scene = context.scene.element; //get scene
     //let client = context.scene.client(); //Get scene client
-    let size = context.scene.size(); //Get scene size
-    let x = event.clientX - size.left - context.scene.node.clientLeft;
-    let y = event.clientY - size.top - context.scene.node.clientTop;
+    let size = scene.size(); //Get scene size
+    //console.log(`${event.clientX} - ${event.clientY}`);
+    //console.log(`${scene.nodes[0].clientLeft} - ${scene.nodes[0].clientTop}`);
+    let x = event.clientX - scene.nodes[0].clientLeft- size.left;
+    let y = event.clientY - scene.nodes[0].clientTop- size.top;
     let hw = context.draw.width.value / 2;
     let hh = context.draw.height.value / 2;
     //Return parsed event
