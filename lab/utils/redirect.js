b692d5d73d6b8659769bf3053b9dc822bdb7036f
Josemi <josemijuanes@gmail.com>
2020-07-09 19:29:09 +0200

    lab: switched to browser redirection and added redirect to sandbox utility

--- lab/utils/redirect.js
+++ lab/utils/redirect.js
@@ -1,7 +1,13 @@
-import {redirectHashbang} from "rouct";
+import {redirectHashbang, redirectBrowser} from "rouct";
 
 //Export redirect wrapper
 export function redirect (to) {
-    return redirectHashbang(to);
+    //return redirectHashbang(to);
+    return redirectBrowser(to);
+}
+
+//Redirect to a sandbox
+export function redirectToSandbox (mode, id) {
+    return redirect(`/editor?${mode}=${encodeURIComponent(id)}`);
 }
 
