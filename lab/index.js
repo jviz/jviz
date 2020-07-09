a431c4d8225c6490d6d80111be32e51ee413fbc5
Josemi <josemijuanes@gmail.com>
2020-07-09 19:29:45 +0200

    lab: updated app component and added global styles

--- lab/index.js
+++ lab/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import * as Router from "rouct";
 import kofi from "kofi";
 
+import {createToaster} from "@siimple/neutrine";
+import {createConfirmer} from "@siimple/neutrine";
+
+import {HomePage} from "./pages/Home/index.js";
+import {NewPage} from "./pages/New/index.js";
 import {EditorPage} from "./pages/Editor/index.js";
+import {initSandboxStorage} from "./utils/sandbox.js";
+import "./style.scss";
 
 //Main app component
 class App extends React.Component {
@@ -13,18 +20,29 @@ class App extends React.Component {
     //Render the app component
     render() {
         return (
-            <Router.HashbangRouter>
+            <Router.BrowserRouter>
                 <Router.Switch>
                     <Router.Route path="/editor" exact component={EditorPage} />
+                    <Router.Route path="/new" exact component={NewPage} />
+                    <Router.Route path="/" exact component={HomePage} />
                 </Router.Switch>
-            </Router.HashbangRouter>
+            </Router.BrowserRouter>
         );
     }
 }
 
 //Mount the app component
 kofi.ready(function () {
-    //Mount app
-    ReactDOM.render(<App />, document.getElementById("root"));
+    //Create the toaster
+    window["toast"] = createToaster({
+        "width": "400px",
+        "align": "right",
+        "time": 1500
+    });
+    window["confirm"] = createConfirmer({});
+    //Initialize sandbox storage
+    return initSandboxStorage().then(function () {
+        ReactDOM.render(<App />, document.getElementById("root")); //Mount app
+    });
 });
 
