0fa8ceffebfb9c666acc6e1cae74f0b870249e35
Josemi <josemijuanes@gmail.com>
2020-07-22 12:11:00 +0200

    render: added orientaation to gradient generator

--- src/render/util/defs.js
+++ src/render/util/defs.js
@@ -1,5 +1,5 @@
 //Create a color gradient
-export function createGradient (parent, name, colors) {
+export function createGradient (parent, name, orientation, colors) {
     let gradient = parent.selectAll(`linearGradient#${name}`);
     if (gradient.length === 0) {
         gradient = parent.append("linearGradient").attr("id", name); //Create the gradient element
@@ -9,6 +9,8 @@ export function createGradient (parent, name, colors) {
     colors.forEach(function (color, index) {
         gradient.append("stop").attr("offset", `${index * gradientStep}%`).attr("stop-color", color);
     });
+    gradient.attr("x2", (orientation === "vertical") ? "0" : "1");
+    gradient.attr("y2", (orientation === "vertical") ? "1" : "0");
     //Return the gradient element
     return gradient;
 }
