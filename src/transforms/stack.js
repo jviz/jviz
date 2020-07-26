47230437a7b226e9b9a4b8e07fb1d3a78bc41ffc
Josemi <josemijuanes@gmail.com>
2020-07-26 19:29:37 +0200

    transforms: rewrite stack transform to use the new partition generator

--- src/transforms/stack.js
+++ src/transforms/stack.js
@@ -1,29 +1,38 @@
 import {isArray} from "../util.js";
+import {generatePartition} from "./util/partition.js";
 //import {propTypes} from "../props.js";
 
 //Default strack generator
-let defaultStackGenerator = function (group, maxGroupValue, field, as) {
+let defaultStackGenerator = function (group, sum, maxSum, field, as) {
     let last = {
         "positive": 0, //Last positive value
         "negative": 0  //Last negative value
     };
-    return group.forEach(function (datum) {
+    return group.map(function (datum) {
         let value = (field !== null) ? datum[field] : defaultProps.value;
         let sign = (value < 0) ? "negative" : "positive";
-        datum[as[0]] = last[sign]; //Set stack start
-        datum[as[1]] = last[sign] + value; //Set stack end
+        let newDatum = Object.assign({}, datum, {
+            [as[0]]: last[sign], //Stack start
+            [as[1]]: last[sign] + value //Stack end
+        });
         last[sign] = last[sign] + value; //Update last value
+        //Return updated datum
+        return newDatum;
     });
 };
 
 //Centered stak generator
-let centerStackGenerator = function (group, maxGroupValue, field, as) {
-    let last = (maxGroupValue - group.sum) / 2;
-    return group.forEach(function (datum) {
+let centerStackGenerator = function (group, sum, maxSum, field, as) {
+    let last = (maxSum - sum) / 2;
+    return group.map(function (datum) {
         let value = (field !== null) ? datum[field] : defaultProps.value;
-        datum[as[0]] = last; //Set stack start
-        datum[as[1]] = last + value; //Set stack end
-        last = last + value
+        let newDatum = Object.assign({}, datum, {
+            [as[0]]: last, //Stack start
+            [as[1]]: last + value //Stack end
+        });
+        last = last + value //Update last
+        //Return updated datum
+        return newDatum;
     });
 };
 
@@ -47,54 +56,34 @@ let defaultProps = {
 //Export stack transform
 export const stackTransform = {
     "transform": function (context, data, props) {
-        let groupby = (typeof props.groupby === "string") ? props.groupby : null; 
+        //let groupby = (typeof props.groupby === "string") ? props.groupby : null; 
         let align = context.value(props.align, null, defaultProps.align); //Get align value
         if (typeof align !== "string" || (typeof align === "string" && typeof stacks[align] === "undefined")) {
             align = defaultProps.align; //Unknown align value ---> set default
         }
         let field = (typeof props.field === "string") ? props.field : null; //Get stack field
-        let groups = []; //Stack groups
-        let maxGroupValue = 0; //Store max value of all groups
-        //Check if no groupby has been provided
-        //console.log(groupby);
-        if (groupby === null || groupby === "") {
-            groups = [data];
-            //Calculate the sum value of the group
-            groups[0].sum = 0;
-            groups[0].forEach(function (datum) {
-                let value = (field !== null) ? Math.abs(datum[props.field]) : defaultProps.value;
-                groups[0].sum = groups[0].sum + value;
+        let as = (isArray(props.as) && props.as.length === 2) ? props.as : defaultProps.as;
+        let {groups, groupby} = generatePartition(data, {"groupby": props.groupby}); //Stack groups
+        //let maxSumValue = 0; //Store max value of all groups
+        let groupSum = []; //To store the grpups sums
+        //Get the sum value of all groups
+        groups.forEach(function (group, index) {
+            groupSum[index] = 0; //Initialize group sum
+            group.forEach(function (datum) {
+                let value = (field !== null) ? Math.abs(datum[field]) : defaultProps.value;
+                groupSum[index] = groupSum[index] + value;
             });
-            //maxGroupValue = groups[0].sum; //Update max value
-        }
-        //Else: groupby value provided --> generate groups
-        else {
-            let maps = {}; //Groups mappings
-            data.forEach(function (datum, index) {
-                let key = datum[groupby];
-                let value = (field !== null) ? Math.abs(datum[props.field]) : defaultProps.value;
-                //Check if this group is not defined
-                if (typeof maps[key] === "undefined") {
-                    groups.push([]);
-                    groups[groups.length - 1].sum = 0; //Initialize group sum value
-                    maps[key] = groups.length - 1;
-                }
-                //Insert this datum
-                groups[maps[key]].push(datum);
-                groups[maps[key]].sum = groups[maps[key]].sum + value; //Math.abs(datum[props.field]);
-            });
-        }
-        //console.log(groups);
-        //Get max value
-        groups.forEach(function (group) {
-            maxGroupValue = Math.max(group.sum, maxGroupValue);
         });
+        let groupMaxSum = Math.max.apply(null, groupSum); //Get max group sums
+        let outputData = []; //Output data object
         //Build the stack for each group
-        groups.forEach(function (group) {
-            stacks[align](group, maxGroupValue, field, defaultProps.as);
+        groups.forEach(function (group, index) {
+            stacks[align](group, groupSum[index], groupMaxSum, field, as).forEach(function (datum) {
+                outputData.push(datum); //Save the new datum object
+            });
         });
-        //Return the data
-        return data;
+        //Return the new data
+        return outputData;
     },
     "props": defaultProps,
     "sourceProps": ["align"]
