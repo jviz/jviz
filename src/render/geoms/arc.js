787d48baed882b58ed6f38b8c14ed2d35e22891e
Josemi <josemijuanes@gmail.com>
2020-07-30 23:46:02 +0200

    render: rewrite arc geom to prevent rendering the arc if an arc property is not valid

--- src/render/geoms/arc.js
+++ src/render/geoms/arc.js
@@ -1,46 +1,66 @@
-import {propTypes, parseProps} from "../../props.js";
+//import {propTypes, parseProps} from "../../props.js";
 import {arc} from "../primitives/arc.js";
+import {isNumber} from "../../util.js";
 
 //Arc default props
 let defaultProps = {
-    "startAngle": propTypes.number(),
-    "endAngle": propTypes.number(),
-    "innerRadius": propTypes.number(),
-    "outerRadius": propTypes.number(),
-    "angle": propTypes.number(),
-    "radius": propTypes.number(),
-    "x": propTypes.number(),
-    "y": propTypes.number()
+    "startAngle": 0,
+    "endAngle": 0,
+    "innerRadius": 0,
+    "outerRadius": 0,
+    "angle": 0,
+    "radius": 0, 
+    "x": 0, 
+    "y": 0
 };
 
 //Export arc geom
 export const arcGeom = {
     "tag": "path",
     "type": "arc",
-    "render": function (context, data, props, element) {
-        //Initialize the arc options
-        let options = parseProps(context, data, props, defaultProps);
-        //Check if user has provided only a radius property
-        if (typeof options.radius === "number") {
-            Object.assign(options, {
-                "innerRadius": 0, 
-                "outerRadius": options.radius
-            });
+    "render": function (context, datum, props, element) {
+        element.attr("d", ""); //Reset arc
+        //Initialize the arc arguments
+        let args = {
+            "centerX": context.value(props.x, datum, null),
+            "centerY": context.value(props.y, datum, null),
+            "innerRadius": 0, //Initial inner radius
+            "outerRadius": null,
+            "startAngle": 0,  //Initial start angle
+            "endAngle": null
+        };
+        //Check for no x or y value provided
+        if (!isNumber(args.centerX) || !isNumber(args.centerY)) {
+            return element; //Not valid values
         }
-        //Check for custom angle
-        if (typeof options.angle === "number") {
-            Object.assign(options, {
-                "startAngle": 0,
-                "endAngle": options.angle
-            });
+        //Check for custom radius
+        if (typeof props.radius !== "undefined" && props.radius !== null) {
+            args.outerRadius = context.value(props.radius, datum, null);
         }
-        //Add the arc center
-        Object.assign(options, {
-            "centerX": options.x,
-            "centerY": options.y
-        });
+        //Check for custom inner and outer radius
+        if (typeof props.innerRadius !== "undefined" && typeof props.outerRadius !== "undefined") {
+            args.innerRadius = context.value(props.innerRadius, datum, null);
+            args.outerRadius = context.value(props.outerRadius, datum, null);
+        }
+        //Check if there is not valid inner or uter radius values
+        if (!isNumber(args.innerRadius) || !isNumber(args.outerRadius)) {
+            return element; //Not valid radius value provided
+        }
+        //Check for single angle value provided
+        if (typeof props.angle !== "undefined") {
+            args.endAngle = context.value(props.angle, datum, null);
+        }
+        //Check if a pair start and end angle has been provided
+        if (typeof props.startAngle !== "undefined" && typeof props.endAngle !== "undefined") {
+            args.startAngle = context.value(props.startAngle, datum, null);
+            args.endAngle = context.value(props.endAngle, datum, null);
+        }
+        //Check if no angle has been provided
+        if (!isNumber(args.startAngle) || !isNumber(args.endAngle)) {
+            return element; //Not valid angle value provided
+        }
         //Build the arc
-        element.attr("d", arc(options));
+        element.attr("d", arc(args));
     },
     "props": defaultProps
 };
