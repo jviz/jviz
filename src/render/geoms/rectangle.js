31e2090db9136e0a0cedc3139b9e9c6625fc00ba
Josemi <josemijuanes@gmail.com>
2020-07-31 10:58:48 +0200

    render: rewrite rectangle geom to check if all attributes of the rectangle are valid

--- src/render/geoms/rectangle.js
+++ src/render/geoms/rectangle.js
@@ -1,61 +1,74 @@
-import {parseProps, propTypes} from "../../props.js";
+import {isNumber} from "../../util.js";
 import {rectangle} from "../primitives/rectangle.js";
 
 //Rectangle default props
 let defaultProps = {
-    "x": propTypes.number(),
-    "y": propTypes.number(),
-    "width": propTypes.number(),
-    "height": propTypes.number(),
-    "x1": propTypes.number(),
-    "y1": propTypes.number(),
-    "x2": propTypes.number(),
-    "y2": propTypes.number(),
-    "xCenter": propTypes.number(),
-    "yCenter": propTypes.number(),
-    "mask": propTypes.boolean(),
-    "radius": propTypes.number()
+    "x": null,
+    "y": null,
+    "width": null,
+    "height": null,
+    "x1": null,
+    "y1": null,
+    "x2": null,
+    "y2": null,
+    "xCenter": null,
+    "yCenter": null,
+    "mask": false,
+    "radius": 0
 };
 
 //Rectangle geom
 export const rectangleGeom = {
     "tag": "path",
     "type": "rectangle",
-    "render": function (context, data, props, element) {
+    "render": function (context, datum, props, element) {
+        element.attr("d", ""); //Reset path
+        //Get rectangle arguments
+        let args = {
+            "radius": context.value(props.radius, datum, defaultProps.radius),
+            "mask": context.value(props.mask, datum, defaultProps.mask)
+        };
         let options = parseProps(context, data, props, defaultProps);
-        //let x = props.x, y = props.y, width = props.width, height = props.height;
-        //Check for x2 values
-        if (typeof options.x2 === "number") {
-            options.width = options.x2 - options.x1;
+        //Check for x1 and x2 values
+        if (typeof props.x1 !== "undefined" && typeof props.x2 !== "undefined") {
+            args.x = context.value(props.x1, datum, null);
+            args.width = context.value(props.x2, datum, null) - args.x1;
         }
-        //Check por x value provided
-        else if (typeof options.x === "number") {
-            options.x1 = options.x;
+        //Check for x and width value provided
+        else if (typeof props.x !== "undefined" && typeof props.width !== "undefined") {
+            args.x = context.value(props.x, datum, null);
+            args.width = context.value(props.width, datum, null);
         }
-        //Chekc if user has provided xCenter values
+        //Check if user has provided xCenter and width values
         else if (typeof options.xCenter === "number" && typeof options.width === "number") {
-            options.x1 = options.xCenter - options.width / 2;
+            args.width = context.value(props.width, datum, null);
+            args.x = context.value(props.xCenter, datum, null) - args.width / 2;
         }
-        //Check for y2 values
-        if (typeof options.y2 === "number") {
-            options.height = options.y2 - options.y1;
+        //Verify if we have x and width valid values --> if not, exit with a warning
+        if (!isNumber(args.x) || !isNumber(args.width)) {
+            return; //context.log.warn("");
         }
-        else if (typeof options.y === "number") {
-            options.y1 = options.y;
+        //Check for y1 and y2 values
+        if (typeof props.y1 !== "undefined" && typeof props.y2 !== "undefined") {
+            args.y = context.value(props.y1, datum, null);
+            args.height = context.value(props.y2, datum, null) - args.y1;
         }
-        //Check if the user has provided yCenter values
+        //Check for y and height value provided
+        else if (typeof props.y !== "undefined" && typeof props.height !== "undefined") {
+            args.y = context.value(props.y, datum, null);
+            args.height = context.value(props.height, datum, null);
+        }
+        //Check if user has provided yCenter and height values
         else if (typeof options.yCenter === "number" && typeof options.height === "number") {
-            options.y1 = options.yCenter - options.height / 2;
+            args.height = context.value(props.height, datum, null);
+            args.y = context.value(props.yCenter, datum, null) - args.height / 2;
+        }
+        //Verify if we have y and height valid values --> if not, exit with a warning
+        if (!isNumber(args.y) || !isNumber(args.height)) {
+            return; //context.log.warn("");
         }
         //Draw the rectangle
-        return element.attr("d", rectangle({
-            "x": options.x1,
-            "y": options.y1,
-            "width": options.width,
-            "height": options.height,
-            "mask": options.mask,
-            "radius": options.radius
-        }));
+        return element.attr("d", rectangle(args));
     },
     "props": defaultProps
 };
