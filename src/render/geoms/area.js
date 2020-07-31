d14df234ed86857d5246b8fd3861407daa226e19
Josemi <josemijuanes@gmail.com>
2020-07-31 10:55:53 +0200

    render: rewrite area geom to prevent rendering invalid points

--- src/render/geoms/area.js
+++ src/render/geoms/area.js
@@ -1,61 +1,71 @@
-import {propTypes} from "../../props.js";
+//import {propTypes} from "../../props.js";
 import {path} from "../path.js";
 import {getCurve} from "../curves/index.js";
+import {isNumber} from "../../util.js";
 
 //Area default props
 let defaultProps = {
-    //"x": propTypes.number(),
-    //"y": propTypes.number(),
-    "x1": propTypes.number(0),
-    "x2": propTypes.number(0),
-    "y1": propTypes.number(0),
-    "y2": propTypes.number(0),
-    "curve": propTypes.string("linear") // linear|catmull-roll|step|step-after|step-before
+    "x": null,
+    "y": null,
+    "x1": 0,
+    "x2": 0,
+    "y1": 0,
+    "y2": 0,
+    "curve": "linear" // linear|catmull-roll|step|step-after|step-before
 };
-
-//Method for drawing an area
-function area (args) {
-    //Initialize the path
-    let data = args.data;
-    let areaPath = path(); //Get the path generator
-    let curve = getCurve(args.curve)(areaPath); //Build the curve generator
-    //Build the forward line of the area
-    for (let i = 0; i < data.length; i++) {
-        curve.point(args.x1(data[i], i), args.y1(data[i], i));
-    }
-    curve.end();
-    //Move reverse
-    for (let i = data.length - 1; i >= 0; i--) {
-        curve.point(args.x2(data[i], i), args.y2(data[i], i));
-    }
-    curve.end(); //End the curve
-    areaPath.close(); //End the path
-    return areaPath.toString();
-}
 
 //Export area geom
 export const areaGeom = {
     "type": "area",
     "tag": "path",
     "render": function (context, data, props, element) {
-        return element.attr("d", area({
-            "x1": function (datum) {
-                return context.value(props.x1, datum, 0);
-            },
-            "x2": function (datum) {
-                return context.value(props.x2, datum, 0);
-            },
-            "y1": function (datum) {
-                return context.value(props.y1, datum, 0);
-            },
-            "y2": function (datum) {
-                return context.value(props.y2, datum, 0)
-            },
-            //Interpolation curve
-            "curve": context.value(props.curve, data[0], defaultProps.curve.defaultValue),
-            //Points to draw
-            "data": data
-        }));
+        element.attr("d", ""); //Reset area path
+        if (data === null || data.length < 2) {
+            return context.log.warn("Area geom needs a valid data object with at least 2 points.");
+        }
+        //Initialize the area arguments
+        let args = {"x1": props.x1, "x2": props.x2, "y1": props.y1, "y2": props.y2};
+        //Check if an x property has been provided --> use it for x1 and x2
+        if (typeof props.x !== "undefined") {
+            Object.assign(args, {"x1": props.x, "x2": props.x});
+        }
+        //Check if an y property has been provided --> use it for y1 and y2
+        if (typeof props.y !== "undefined") {
+            Object.assign(args, {"y1": props.y, "y2": props.y});
+        }
+        //Add and validate the curve value
+        args.curve = context.value(props.curve, data[0], defaultProps.curve);
+        if (typeof args.curve !== "string" || args.curve.length === 0){
+            return context.log.warn("Invalid curve value provided to area geom");
+        }
+        //Build the area
+        let areaPath = path(); //Get the path generator
+        let curve = getCurve(args.curve)(areaPath); //Initialize the curve generator
+        //Build the forward line of the area
+        for (let i = 0; i < data.length; i++) {
+            let x = context.value(args.x1, data[i], null); //Get x value
+            let y = context.value(args.y1, data[i], null); //Get y value
+            if (!isNumber(x) || !isNumber(y)) {
+                return; context.log.warn(`Invalid pair (x,y) provided at position '${i}' of the area geom`);
+            }
+            //Add this point
+            curve.point(x, y);
+        }
+        curve.end(); //End the top curve
+        //Move reverse
+        for (let i = data.length - 1; i >= 0; i--) {
+            let x = context.value(args.x2, data[i], null); //Get x value
+            let y = context.value(args.y2, data[i], null); //Get y value
+            if (!isNumber(x) || !isNumber(y)) {
+                return; context.log.warn(`Invalid pair (x,y) provided at position '${i}' of the area geom`);
+            }
+            //Add this point
+            curve.point(x, y);
+        }
+        curve.end(); //End the bottom curve
+        areaPath.close(); //End the path
+        //Return the area path
+        return element.attr("d", areaPath.toString());
     },
     "props": defaultProps
 };
