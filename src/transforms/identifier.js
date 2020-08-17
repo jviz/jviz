ba86162edf9549b1e6b88696f1f7041a326f6b2e
Josemi <josemijuanes@gmail.com>
2020-08-17 16:01:12 +0200

    transforms: added groupby option to identifier transform

--- src/transforms/identifier.js
+++ src/transforms/identifier.js
@@ -1,16 +1,48 @@
-import {propTypes} from "../props.js";
+import {isString} from "../util.js";
+
+//Default transform props
+let defaultProps = {
+    "groupby": null,
+    "as": "index"
+};
 
 //Export identifier transform properties
 export const identifierTransform = {
     "transform": function (context, data, props) {
-        return data.map(function (datum, index) {
-            return Object.assign(datum, {
-                [props.as]: index  //Add the index as an identifier of this datum
+        let groupby = (typeof props.groupby === "string") ? props.groupby.trim() : null; 
+        let as = isString(props.as) ? props.as : defaultProps.as;
+        let groups = [];
+        //Build groups
+        if (groupby !== null) {
+            let maps = {}; //Groups mappings
+            data.forEach(function (datum, index) {
+                let key = datum[groupby];
+                //Check if this group is not defined
+                if (typeof maps[key] === "undefined") {
+                    groups.push([]);
+                    maps[key] = groups.length - 1;
+                }
+                //Insert this datum
+                groups[maps[key]].push(datum);
+            });
+        }
+        //No groupby property provided --> use only one group
+        else {
+            groups = [data]; //Register only one group with all elements
+        }
+        //Add the index for all datum elements
+        let newData = []; //New data list
+        groups.forEach(function (group) {
+            return group.forEach(function (datum, index) {
+                return newData.push(Object.assign({}, datum, {
+                    [as]: index //Save the index of the datum in the list
+                }));
             });
         });
+        //Return the new data object
+        return newData;
     },
-    "props": {
-        "as": propTypes.string()
-    }
+    "props": defaultProps,
+    "sourceProps": []
 };
 
