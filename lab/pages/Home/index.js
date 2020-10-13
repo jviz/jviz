a15ec56e53cbaff36cb4eb50e792eee413d6d5d8
Josemi <josemijuanes@gmail.com>
2020-10-13 13:43:00 +0200

    lab: redesigned Home view for displaying local and examples sandboxes

--- lab/pages/Home/index.js
+++ lab/pages/Home/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import kofi from "kofi";
 
-import {If, ForEach} from "@siimple/neutrine";
-import {Content} from "@siimple/neutrine";
-import {Row, Column} from "@siimple/neutrine";
-import {Btn} from "@siimple/neutrine";
-import {Spinner} from "@siimple/neutrine";
-import {Tabs, TabsItem} from "@siimple/neutrine";
+import {If, ForEach, Renderer} from "neutrine/lib/components";
+import {Content} from "neutrine/lib/components";
+//import {Row, Column} from "neutrine/lib/components";
+import {Btn, Rule} from "neutrine/lib/components";
+import {Spinner} from "neutrine/lib/components";
+import {Nav, NavItem, NavGroup} from "neutrine/lib/components";
+import {Navside} from "neutrine/lib/components";
+import {Heading} from "neutrine/lib/components";
+import {Media, MediaContent, MediaEnd} from "neutrine/lib/components";
+import {Input} from "neutrine/lib/components";
 
 import {ListSandboxes} from "../../components/ListSandboxes/index.js";
-import {Splash} from "../../components/Splash/index.js";
+//import {Splash} from "../../components/Splash/index.js";
 import {loadLocalSandboxes, loadRemoteSandboxes} from "../../utils/sandbox.js";
 import {redirect, redirectToSandbox} from "../../utils/redirect.js";
 import style from "./style.scss";
@@ -94,44 +98,58 @@ export class HomePage extends React.Component {
     render() {
         let self = this;
         let hasSandboxes = this.state.sandboxes.length > 0; // && this.state.loading === false;
+        let title = tabsList[this.state.mode].title; //Get title
         return (
-            <Content size="xlarge">
-                <Row className="siimple--mb-0">
-                    {/* Left column --> presentation and links */}
-                    <Column size="5" align="center">
-                        <div className={style.title}>
+            <div className={style.root}>
+                {/* Left column --> presentation and links */}
+                <Navside>
+                    {/* Editor title */}
+                    <div className="siimple--mb-5" align="center">
+                        <span className={style.title}>
                             <strong>jviz</strong>lab
-                        </div>
-                        {/* Create a new sandbox */}
-                        <div className="siimple--mt-5" align="center">
-                            <Btn color="light" onClick={this.handleNewClick}>
-                                Create a new <strong>sandbox</strong>
-                            </Btn>
-                        </div>
-                    </Column>
-                    {/* Right column --> content */}
-                    <Column size="7">
-                        {/* Display tabs */}
-                        <Tabs className="siimple--mb-3">
-                            <ForEach items={Object.keys(tabsList)} render={function (key, index) {
-                                let item = tabsList[key];
-                                return React.createElement(TabsItem, {
-                                    "active": self.state.mode === key,
-                                    "onClick": function () {
-                                        return self.handleModeChange(key);
-                                    },
-                                    "key": index
-                                }, item.title);
-                            }} />
-                        </Tabs>
+                        </span>
+                    </div>
+                    {/* Display tabs sources links */}
+                    <Nav vertical className="siimple--mb-3">
+                        <NavGroup>Sandboxes</NavGroup>
+                        <ForEach items={Object.keys(tabsList)} render={function (key, index) {
+                            let item = tabsList[key];
+                            return React.createElement(NavItem, {
+                                ///"className": "siimple--pl-0 siimple--pr-0",
+                                "active": self.state.mode === key,
+                                "onClick": function () {
+                                    return self.handleModeChange(key);
+                                },
+                                "key": index
+                            }, item.title);
+                        }} />
+                    </Nav>
+                    <Rule />
+                    {/* Create a new sandbox */}
+                    <Btn color="secondary" fluid onClick={this.handleNewClick}>
+                        <strong>Create new</strong>
+                    </Btn>
+                </Navside>
+                {/* Right column --> content */}
+                <div style={{"flexGrow":"1"}}>
+                    <Content size="medium">
+                        {/* Title and filter */}
+                        <Media className="siimple--mb-3">
+                            <MediaContent>
+                                <Heading type="h4" className="siimple--mb-0">{title}</Heading>
+                            </MediaContent>
+                            <MediaEnd>
+                                <Input type="text" placeholder="Filter..." />
+                            </MediaEnd>
+                        </Media>
                         {/* Loading sandboxes */}
                         <If condition={this.state.loading}>
-                            <Splash icon={null}>
-                                <Spinner className="siimple--mb-2" color="dark" />
+                            <div align="center" className="siimple--mt-8">
+                                <Spinner className="siimple--mb-2" color="secondary" />
                                 <div className="siimple--text-bold" align="center">
                                     Loading sandboxes...
                                 </div>
-                            </Splash>
+                            </div>
                         </If>
                         {/* List sandboxes */}
                         <If condition={!this.state.loading} render={function () {
@@ -140,9 +158,9 @@ export class HomePage extends React.Component {
                                 "onClick": self.handleSandboxClick
                             });
                         }} />
-                    </Column>
-                </Row>
-            </Content>
+                    </Content>
+                </div>
+            </div>
         );
     }
 }
