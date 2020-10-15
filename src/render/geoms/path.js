b2b30c68e2369b60d764e1c07ae6ab09298b8ea7
Josemi <josemijuanes@gmail.com>
2020-10-15 20:51:06 +0200

    render: added x and y props to path geom

--- src/render/geoms/path.js
+++ src/render/geoms/path.js
@@ -1,5 +1,9 @@
+import {isNumber} from "../../util.js";
+
 //Path default props
 let defaultProps = {
+    "x": null,
+    "y": null,
     "path": ""
 };
 
@@ -8,7 +12,13 @@ export const pathGeom = {
     "tag": "path",
     "type": "path",
     "render": function (context, datum, props, element) {
+        element.attr("transform", ""); //Reset transform
         let path = context.value(props["path"], datum, defaultProps.path); //Get path value
+        let x = context.value(props["x"], datum, null);
+        let y = context.value(props["y"], datum, null);
+        if (isNumber(x) && isNumber(y)) {
+            element.attr("transform", `translate(${x},${y})`); //Translate the path
+        }
         //Return the element with the path attribute
         return element.attr("d", path);
     },
