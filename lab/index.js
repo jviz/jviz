0ceced08bddbab34b29789425373c5d205630b30
Josemi <josemijuanes@gmail.com>
2020-10-15 21:22:13 +0200

    lab: redesigned main App component and fixed imports

--- lab/index.js
+++ lab/index.js
@@ -2,23 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as Router from "rouct";
 import kofi from "kofi";
+import {createToaster} from "neutrine/lib/widgets";
+import {createConfirmer} from "neutrine/lib/widgets";
+import {whenReady} from "neutrine/lib/utils";
 
-import {createToaster} from "@siimple/neutrine";
-import {createConfirmer} from "@siimple/neutrine";
+//Import global styles
+import "siimple/dist/siimple.css";
+import "siimple-experiments/dist/siimple-experiments.css";
+import "siimple-icons/dist/siimple-icons.css";
+import "neutrine/lib/components.css";
+import "neutrine/lib/editor.css";
+import "neutrine/lib/widgets.css";
 
 import {HomePage} from "./pages/Home/index.js";
 import {NewPage} from "./pages/New/index.js";
 import {EditorPage} from "./pages/Editor/index.js";
 import {initSandboxStorage} from "./utils/sandbox.js";
+import {Splash} from "./components/Splash/index.js";
 import "./style.scss";
 
 //Main app component
 class App extends React.Component {
     constructor(props) {
         super(props);
+        //Initial lab state
+        this.state = {
+            "loading": true
+        };
+    }
+    //Component did mount --> init sandbox storage
+    componentDidMount() {
+        let self = this;
+        return initSandboxStorage().then(function () {
+            return kofi.delay(1000);
+        }).then(function () {
+            return self.setState({"loading": false});
+        });
     }
     //Render the app component
     render() {
+        //Check for loading app state --> display loading splash
+        if (this.state.loading === true) {
+            return React.createElement(Splash, {});
+        }
+        //Render the lab router
         return (
             <Router.BrowserRouter>
                 <Router.Switch>
@@ -32,7 +59,7 @@ class App extends React.Component {
 }
 
 //Mount the app component
-kofi.ready(function () {
+whenReady(function () {
     //Create the toaster
     window["toast"] = createToaster({
         "width": "400px",
@@ -40,9 +67,6 @@ kofi.ready(function () {
         "time": 1500
     });
     window["confirm"] = createConfirmer({});
-    //Initialize sandbox storage
-    return initSandboxStorage().then(function () {
-        ReactDOM.render(<App />, document.getElementById("root")); //Mount app
-    });
+    ReactDOM.render(<App />, document.getElementById("root")); //Mount app
 });
 
