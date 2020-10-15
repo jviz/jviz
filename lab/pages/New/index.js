fd2a25f25275337a94a5104fb3c18cf8f042b6f6
Josemi <josemijuanes@gmail.com>
2020-10-15 21:21:42 +0200

    lab: redesigned new sandbox page

--- lab/pages/New/index.js
+++ lab/pages/New/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import kofi from "kofi";
 
-import {Spinner} from "@siimple/neutrine";
+import {If} from "neutrine/lib/components";
+import {Content} from "neutrine/lib/components";
+import {Spinner} from "neutrine/lib/components";
+import {Input} from "neutrine/lib/components";
+import {Btn} from "neutrine/lib/components";
+import {Field, FieldLabel, FieldHelper} from "neutrine/lib/components";
 
-import {Splash} from "../../components/Splash/index.js";
 import {createSandbox, saveLocalSandbox} from "../../utils/sandbox.js";
 import {redirect, redirectToSandbox} from "../../utils/redirect.js";
 
@@ -11,29 +15,53 @@ import {redirect, redirectToSandbox} from "../../utils/redirect.js";
 export class NewPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            "loading": false
+        };
+        //Referenced elements
+        this.ref = {
+            "name": React.createRef()
+        };
+        //Bind methods
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    //Component did mount ---> create the new sandbox
-    componentDidMount() {
+    //Handle submit --> create the new sandbox
+    handleSubmit() {
         let self = this;
-        //let newSandbox = createSandbox({}); //Create a new sandbox
-        return kofi.delay(1000).then(function () {
-            return saveLocalSandbox(createSandbox({}));
-        }).then(function (newSandbox) {
-            //console.log("sandbox created");
-            return redirectToSandbox("sandbox", newSandbox.id);
+        return this.setState({"loading": true}, function () {
+            //let newSandbox = createSandbox({}); //Create a new sandbox
+            return kofi.delay(1000).then(function () {
+                return saveLocalSandbox(createSandbox({
+                    "name": self.ref.name.current.value.trim()
+                }));
+            }).then(function (newSandbox) {
+                //console.log("sandbox created");
+                return redirectToSandbox("sandbox", newSandbox.id);
+            });
         });
     }
     //Render the new sandbox page
     render() {
         return (
-            <Splash icon={null}>
-                <div className="siimple--mb-2">
-                    <Spinner color="dark" />
+            <Content size="small">
+                {/* New sandbox name */}
+                <Field>
+                    <FieldLabel>Sandbox name</FieldLabel>
+                    <Input type="text" fluid ref={this.ref.name} defaultValue="New sandbox" />
+                    <FieldHelper>Name of your new sandbox</FieldHelper>
+                </Field>
+                {/* Create the sandbox */}
+                <div align="center">
+                    <If condition={this.state.loading} render={function () {
+                        return React.createElement(Spinner, {"color": "primary"});
+                    }} />
+                    <If condition={!this.state.loading}>
+                        <Btn fluid color="success" onClick={this.handleSubmit}>
+                            <strong>Create sandbox</strong>
+                        </Btn>
+                    </If>
                 </div>
-                <div className="siimple--mt-0">
-                    <strong>Creating a new sandbox...</strong>
-                </div>
-            </Splash>
+            </Content>
         );
     }
 }
