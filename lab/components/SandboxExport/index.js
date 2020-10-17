7255ab811b719eabc8a1dabe33b8a78fae0bd42c
Josemi <josemijuanes@gmail.com>
2020-10-17 14:05:43 +0200

    lab: redesigned SandboxExport component to allow exporting a single schema file from the sandbox

--- lab/components/SandboxExport/index.js
+++ lab/components/SandboxExport/index.js
@@ -5,14 +5,14 @@ import {Icon} from "neutrine/lib/components";
 import {ModalWrapper, ModalBody, ModalFooter} from "neutrine/lib/components";
 import {Placeholder, PlaceholderGroup} from "neutrine/lib/components";
 import {Field, FieldLabel, FieldHelper} from "neutrine/lib/components";
-import {Input} from "neutrine/lib/components";
+import {Input, Select, Switch, Label} from "neutrine/lib/components";
 
-import {exportSandbox} from "../../utils/sandbox.js";
+import {getSandboxFile} from "../../utils/sandbox.js";
 
 //Export types
 let exportTypes = {
-    "schema": {"title": "Export as JSON Schema"},
-    "sandbox": {"title": "Export as JSON Sandbox"}
+    "sandbox": {"title": "Export full sandbox"},
+    "schema": {"title": "Export schema"}
 };
 
 //Generate the filename
@@ -21,11 +21,11 @@ let buildFilename = function (sandbox) {
 };
 
 //Download a file
-let downloadFile = function (exportObj, exportName){
-    let data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+let downloadFile = function (content, name){
+    let data = "data:text/json;charset=utf-8," + encodeURIComponent(content);
     let downloadAnchorNode = document.createElement("a");
     downloadAnchorNode.setAttribute("href", data);
-    downloadAnchorNode.setAttribute("download", exportName); // + ".json");
+    downloadAnchorNode.setAttribute("download", name); // + ".json");
     document.body.appendChild(downloadAnchorNode); //Required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove(); //Remove element
@@ -37,21 +37,17 @@ export class SandboxExport extends React.Component {
         super(props);
         //Initial state
         this.state = {
-            "type": "schema",
-            "defaultFilename": buildFilename(props.sandbox)
+            "type": "sandbox"
         };
         //Referenced elements
         this.ref = {
-            "filename": React.createRef()
+            "sandboxName": React.createRef(),
+            "schemaFile": React.createRef(),
+            "schemaJoinData": React.createRef()
         };
         //Bind methods
         this.handleTypeChange = this.handleTypeChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.getFilename = this.getFilename.bind(this);
-    }
-    //Get the filename
-    getFilename() {
-        return this.state.defaultFilename;
     }
     //Handle type change
     handleTypeChange(key) {
@@ -60,27 +56,75 @@ export class SandboxExport extends React.Component {
     //Handle submit
     handleSubmit() {
         let self = this;
-        //Build export options
-        let exportOptions = {
-            "mode": this.state.type,
-            "filename": this.getFilename()
-        };
+        let sandbox = this.props.sandbox;
+        let exportPromise = null; //Promise to get data to export
+        //Check for exporting full sandbox
+        if (this.state.type === "sandbox") {
+            exportPromise = Promise.resolve({
+                "content": JSON.stringify(sandbox),
+                "name": this.ref.sandboxName.current.value
+            });
+        }
+        //Check for exporting individual schema
+        else if (this.state.type === "schema") {
+            let file = this.ref.schemaFile.current.value; //File to export
+            exportPromise = Promise.resolve({
+                "content": getSandboxFile(sandbox, file),
+                "name": file
+            });
+        }
         //Handle export
-        return exportSandbox(this.props.sandbox, exportOptions).then(function (content) {
-            downloadFile(content, exportOptions.filename); //Download file
-            return self.props.onClose();
+        return exportPromise.then(function (options) {
+            downloadFile(options.content, options.name);
+            return self.props.onClose(); //Close export dialog
         });
+    }
+    //Render sandbox export options
+    renderSandboxExport() {
+        let self = this;
+        let defaultName = buildFilename(this.props.sandbox);
+        return (
+            <React.Fragment>
+                {/* Sandbox output name */}
+                <Field>
+                    <FieldLabel>File name</FieldLabel>
+                    <Input type="text" ref={this.ref.sandboxName} fluid defaultValue={defaultName} />
+                    <FieldHelper>Type the name of the exported file</FieldHelper>
+                </Field>
+            </React.Fragment>
+        );
+    }
+    //Render schema export options
+    renderSchemaExport() {
+        let self = this;
+        let files = Object.keys(this.props.sandbox.files).filter(function (file) {
+            return self.props.sandbox.files[file].type === "schema";
+        });
+        return (
+            <React.Fragment>
+                {/* Schema to export */}
+                <Field>
+                    <FieldLabel>Schema to export</FieldLabel>
+                    <Select fluid ref={this.ref.schemaFile} defaultValue={files[0]}>
+                        <ForEach items={files} render={function (file) {
+                            return <option value={file} key={file}>{file}</option>;
+                        }} />
+                    </Select>
+                    <FieldHelper>Select a schema from your sandbox to export</FieldHelper>
+                </Field>
+            </React.Fragment>
+        );
     }
     //Render the export component
     render() {
         let self = this;
-        let typesList = Object.keys(exportTypes);
+        let types = Object.keys(exportTypes);
         return (
-            <ModalWrapper size="small" title="Export as" onClose={this.props.onClose}>
+            <ModalWrapper size="small" title="Export sandbox" onClose={this.props.onClose}>
                 <ModalBody className="siimple--bg-light1">
                     {/* Export type */}
                     <PlaceholderGroup className="siimple--flex siimple--flex-row">
-                        <ForEach items={typesList} render={function (key, index) {
+                        <ForEach items={types} render={function (key, index) {
                             let type = exportTypes[key];
                             let onClick = function () {
                                 return self.handleTypeChange(key);
@@ -94,12 +138,14 @@ export class SandboxExport extends React.Component {
                             );
                         }} />
                     </PlaceholderGroup>
-                    {/* Filename */}
-                    <Field>
-                        <FieldLabel>File name</FieldLabel>
-                        <Input type="text" ref={this.ref.filename} fluid placeholder={this.state.defaultFilename} />
-                        <FieldHelper>Type the name of the exported file</FieldHelper>
-                    </Field>
+                    {/* Render sandbox export options */}
+                    <If condition={this.state.type === "sandbox"} render={function () {
+                        return self.renderSandboxExport();
+                    }} />
+                    {/* Render schema export options */}
+                    <If condition={this.state.type === "schema"} render={function () {
+                        return self.renderSchemaExport();
+                    }} />
                 </ModalBody>
                 {/* Submit export */}
                 <ModalFooter className="siimple--pt-0 siimple--bg-light1">
