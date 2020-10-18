974457f337352477e27c7fdf8284e201aac42ffa
Josemi <josemijuanes@gmail.com>
2020-10-18 11:19:31 +0200

    lab: redesigned some sandbox utils methods and added methods to run a sandbox, export a sandbox and supported file types

--- lab/utils/sandbox.js
+++ lab/utils/sandbox.js
@@ -1,4 +1,5 @@
 import kofi from "kofi";
+//import jviz from "jviz";
 import lzString from "lz-string";
 import {createDatabase} from "./database.js";
 
@@ -11,6 +12,12 @@ export function initSandboxStorage () {
         db.createObjectStore("sandbox", {"keyPath": "id"});
     });
 }
+
+//Export available sandbox files
+export const sandboxFileTypes = {
+    "schema": {"name": "Jviz schema", "icon": "code"},
+    "json": {"name": "JSON", "icon": "file"}
+};
 
 //Encode sandbox file
 export function encodeSandboxFile (content) {
@@ -36,9 +43,9 @@ export function createSandbox (initSandbox) {
         "description": "",
         "author": "",
         "files": {
-            "schema.json": createSandboxFile("schema.json", "")
+            "schema.jviz": createSandboxFile("schema", "")
         },
-        "main": "schema.json",
+        "main": "schema.jviz",
         "runConfig": null,
         "thumbnail": null,
         "created_at": Date.now(),
@@ -60,9 +67,9 @@ export function validateSandbox (sandbox) {
         Object.assign(sandbox, {
             "version": "2",
             "files": {
-                "schema.json": createSandboxFile("schema", sandbox["schema"] + "")
+                "schema.jviz": createSandboxFile("schema", sandbox["schema"] + "")
             },
-            "main": "schema.json",
+            "main": "schema.jviz",
             "runConfig": null
         });
         //Remove some fields
@@ -82,10 +89,83 @@ export function parseSandbox (sandbox, currentFile) {
     });
 }
 
-//Get the content of a file
-export function getSandboxFile (sandbox, file) {
-    return decodeSandboxFile(sandbox.files[file].content);
+//Export a sandbox file --> get the content of the file
+export function exportSandboxFile (sandbox, file) {
+    return Promise.resolve(decodeSandboxFile(sandbox.files[file].content));
 }
+
+//Export a sandbox schema --> export as json with the data injected
+export function exportSandboxSchema (sandbox, file) {
+    return new Promise(function (resolve, reject) {
+        //TODO: check if this file is a schema
+        let schema = JSON.parse(decodeSandboxFile(sandbox.files[file].content));
+        //Iterate in data field --> find imports of other sandbox files
+        if (typeof schema.data === "object" && Array.isArray(schema.data)) {
+            for (let i = 0; i < schema.data.length; i++) {
+                //Check if data is not using a sandbox file
+                if (typeof schema.data[i].url !== "string" || !schema.data[i].url.startsWith("sandbox://")) {
+                    continue; //Nothing to import here
+                }
+                //Get the file to import
+                let dataFile = schema.data[i].url.replace("sandbox://", "");
+                if (typeof sandbox.files[dataFile] === "undefined") {
+                    return reject(new Error(`Error in '${file}': sandbox file '${dataFile}' not found`));
+                }
+                //else if (sandbox.files[dataFile].type !== "data") {
+                //    return reject(new Error(`Error in '${file}': loading a non-data file '${dataFile}'.`));
+                //}
+                //Get data content
+                let data = JSON.parse(decodeSandboxFile(sandbox.files[dataFile].content));
+                if (typeof schema.data[i].format === "object" && typeof schema.data[i].format.field === "string") {
+                    data = jviz.util.nest(data, schema.data[i].format.field);
+                }
+                //Replace in data object
+                schema.data[i].value = data;
+                delete schema.data[i].url; //Remove old url field
+                delete schema.data[i].format; //Remove format field
+            }
+        }
+        //Done --> return schema in json with the data injected
+        return resolve(schema);
+    });
+}
+
+//Get a list of schema files to run
+let getSchemaFilesForRunning = function (sandbox, currentFile) {
+    //Check for custom run configuration
+    if (Array.isArray(sandbox.runConfig) && sandbox.runConfig !== null) {
+        return sandbox.runConfig;
+    }
+    //Default --> return only the current file
+    return [currentFile];
+};
+
+//Run sandbox
+export function runSandbox (sandbox, currentFile, callback) {
+    return new Promise(function (resolve, reject) {
+        //Get the list of schema files to execute
+        let files = getSchemaFilesForRunning(sandbox, currentFile);
+        if (files.length === 0) {
+            return reject(new Error("No schema files to run"));
+        }
+        //Method to execute each file
+        let runSandboxSchema = function (index) {
+            if (index >= files.length) {
+                return resolve(null); //Run finished
+            }
+            //Export this file
+            exportSandboxSchema(sandbox, files[index]).then(function (schema) {
+                return callback(files[index], schema, index);
+            }).then(function () {
+                return runSandboxSchema(index + 1);
+            }).catch(function (error) {
+                return reject(error);
+            });
+        };
+        //Start parsing all schemas
+        return runSandboxSchema(0);
+    });
+};
 
 //Load user sandboxes
 export function loadLocalSandboxes () {
