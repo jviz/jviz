37ebb6f68143dad8f7dbe1181a7e877dfb92dd8c
Josemi <josemijuanes@gmail.com>
2020-10-19 11:48:28 +0200

    lab: rewrite ExplorePanel component --> switched to cell system for rendering multiple schemas

--- lab/components/SandboxEditor/ExplorePanel/index.js
+++ lab/components/SandboxEditor/ExplorePanel/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import kofi from "kofi";
-import {If, Renderer} from "neutrine/lib/components";
+import {If, Renderer, ForEach} from "neutrine/lib/components";
 
 import {Preview} from "./Preview/index.js";
+import {PreviewCell, CellElement} from "./Preview/Cell.js";
 import {Console} from "./Console/index.js";
 import {ConsoleLogsTab} from "./Console/LogsTab.js";
 import {ConsoleStateTab} from "./Console/StateTab.js";
+import {ConsoleSchemaTab} from "./Console/SchemaTab.js";
 import {runSandbox} from "../../../utils/sandbox.js";
 import style from "./style.scss";
 
@@ -15,80 +17,117 @@ export class ExplorePanel extends React.Component {
         super(props);
         //Initial state
         this.state = {
-            "consoleTab": "logs",
-            "deployed": false,
-            "ready": false //Plot is ready to interact
+            "currentTab": "logs",
+            "currentCell": -1,
+            "totalCells": 0
         };
-        //Referenced elements
-        this.ref = {
-            "parent": React.createRef()
-        };
-        this.schema = null; //Deployed schema
-        this.viewer = {}; //jviz viewers instance
-        this.logs = [];
+        //Cells elements
+        this.cells = [];
         //Bind methods
         this.run = this.run.bind(this);
-        this.clear = this.clear.bind(this);
-        this.log = this.log.bind(this);
-        this.handleConsoleTabChange = this.handleConsoleTabChange.bind(this);
+        //this.getViewer = this.getViewer.bind(this);
+        this.addCell = this.addCell.bind(this);
+        this.getCell = this.getCell.bind(this);
+        this.getCurrentCell = this.getCurrentCell.bind(this);
+        this.setCurrentCell = this.setCurrentCell.bind(this);
+        this.handleTabChange = this.handleTabChange.bind(this);
         this.handleStateChange = this.handleStateChange.bind(this);
+        this.handleClear = this.handleClear.bind(this);
     }
     //Component will unmount
     componentWillUnmount() {
-        return this.clear();
+        //return this.handleClear();
     }
     //Handle console tab change
-    handleConsoleTabChange(newTab) {
-        return this.setState({"consoleTab": newTab});
+    handleTabChange(newTab) {
+        return this.setState({"currentTab": newTab});
     }
     //Handle state variable change
-    handleStateChange(file, name, value) {
-        this.viewer[file].state(name, value);
-        this.viewer[file].render(); //Render the plot
+    handleStateChange(name, value) {
+        let currentCell = this.getCurrentCell(); //Get current cell
+        currentCell.viewer.state(name, value);
+        currentCell.viewer.render(); //Render the plot
     }
-    //Register a log message
-    log(type, message) {
-        this.logs.push({
-            "type": type, 
-            "message": message,
-            "date": Date.now()
+    //Clear the preview
+    handleClear() {
+        //Destroy all cells and clear the list of available cells
+        this.cells = this.cells.filter(function (cell) {
+            if (cell.viewer !== null) {
+                cell.viewer.destroy();
+            }
+            return false;
+        });
+        //Remove the index of the current viewer and reset cells
+        return this.setState({
+            "totalCells": 0,
+            "currentCell": -1
+        });
+    }
+    //Get a cell element
+    getCell(index) {
+        return this.cells[index];
+    }
+    //Get the current cell
+    getCurrentCell() {
+        return this.cells[this.state.currentCell];
+    }
+    //Set a new current cell
+    setCurrentCell(index, callback) {
+        //Update the state with the current cell
+        let newState = {
+            "currentCell": index,
+            "currentTab": "logs"
+        };
+        return this.setState(newState, callback);
+    }
+    //Add a new cell element
+    addCell(file, callback) {
+        let self = this;
+        //Create a new cell object
+        this.cells.push(new CellElement(file));
+        let newState = {
+            "totalCells": this.state.totalCells + 1,
+            "currentCell": this.state.totalCells,
+            "currentTab": "logs"
+        };
+        //Update the state and call the provided callback
+        return this.setState(newState, function () {
+            return callback();
         });
     }
     //Run content
     run(sandbox, currentFile) {
         let self = this;
-        this.logs = []; //Clear logs messages
+        let cell = null;
         return new Promise(function (resolve, reject) {
-            self.clear(); //Remove the current plot
-            self.log("info", "Run started");
-            let newState = {
-                "consoleTab": "logs", //Return to logs tab
-                "deployed": true,
-                "ready": false
-            };
-            //Update the new state
-            return self.setState(newState, function () {
-                return runSandbox(sandbox, currentFile, function (name, schema, index) {
-                    self.log("info", `Running '${name}'`);
-                    //console.log(schema); //Print schema
+            //Set the current cell to the new cell
+            return self.addCell(currentFile, function () {
+                cell = self.getCurrentCell(); //Get the current cell
+                cell.log("info", "Run started");
+                cell.log("info", `Running '${currentFile}'`);
+                return runSandbox(sandbox, currentFile).then(function (schema) {
                     //window.currentSchema = schema; //Save current schema
                     //self.schema = schema; //Save current schema
                     //Create the viewer
-                    self.viewer[name] = jviz(schema, {
-                        "parent": self.ref.parent.current
+                    cell.schema = schema; //Set cell schema
+                    cell.viewer = jviz(schema, {
+                        "parent": cell.parent.current
                     });
                     //console.log(self.viewer); //Print viewer instance
-                    return self.viewer[name].render();
+                    return cell.viewer.render();
                 }).then(function () {
-                    window.viewer = self.viewer; //Save the viewer instance in the window object
+                    //window.viewer = self.viewer; //Save the viewer instance in the window object
                     //Update the state
-                    return self.setState({"ready": true}, function () {
-                        self.log("info", "Run finished"); //Display in logs
-                        return resolve(); //Ready
-                    });
+                    //let currentIndex  = Math.max(0, self.state.currentViewerIndex);
+                    //return self.setState({"currentViewerIndex": currentIndex}, function () {
+                    cell.log("info", "Run finished"); //Display in logs
+                    return resolve(); //Ready
                 }).catch(function (error) {
                     console.error(error);
-                    self.log("error", error.message); //Save the error in logs
+                    cell.log("error", error.message); //Save the error in logs
+                    cell.error = error.message; //Set error message
+                    cell.viewer = null; //Delete cell viewer
+                    //TODO: display error in cell
                     //Reject the promise, but the error will be displayed in the explorer console
                     //instead of in js console
                     return reject(error);
@@ -96,53 +135,57 @@ export class ExplorePanel extends React.Component {
             });
         });
     }
-    //Clear the preview
-    clear() {
-        let self = this;
-        Object.keys(this.viewer).forEach(function (file) {
-            return self.viewer[file].destroy(); //Remove bounds and elements
-        });
-        //Remove all references to the old viewers
-        this.viewer = {};
-    }
     //Render preview panel
     render() {
         let self = this;
+        let currentCell = self.state.currentCell; //Get current index
         return (
             <div className={style.root}>
                 {/* Render the preview panel */}
                 <div className={style.primaryItem}>
-                    <Renderer render={function () {
-                        return React.createElement(Preview, {
-                            "ref": self.ref.parent,
-                            "deployed": self.state.deployed
-                        });
-                    }} />
+                    <Preview onClear={this.handleClear}>
+                        <ForEach items={this.cells} render={function (cell, index) {
+                            return React.createElement(PreviewCell, {
+                                "key": index,
+                                "ref": cell.parent,
+                                "active": index === currentCell,
+                                "index": index,
+                                "error": cell.error,
+                                "onClick": self.setCurrentCell
+                            });
+                        }} />
+                    </Preview>
                 </div>
                 {/* Render the console panel */}
-                <If condition={this.state.deployed} render={function () {
-                    let ready = self.state.ready;
-                    let tab = self.state.consoleTab; //Get current tab
+                <If condition={currentCell > -1} render={function () {
+                    let tab = self.state.currentTab; //Get current tab
                     //let tabs = (self.state.ready) ? ["logs", "state", "data"] : ["logs"]; //Available tabs
-                    let tabs = ["logs", "state"]; //At this momento only logs tab is enabled
+                    let tabs = ["logs", "state", "schema"]; //At this moment only logs tab is enabled
+                    let title = `[${currentCell}] ${self.getCurrentCell().file}`; //Get console title
                     //Return console panel
                     return (
                         <div className={style.secondaryItem}>
-                            <Console currentTab={tab} tabs={tabs} onChange={self.handleConsoleTabChange}>
+                            <Console currentTab={tab} tabs={tabs} onChange={self.handleTabChange} title={title}>
                                 {/* Render logs tab */}
                                 <If condition={tab === "logs"} render={function () {
                                     return React.createElement(ConsoleLogsTab, {
-                                        "logs": self.logs
+                                        "logs": self.getCurrentCell().logs
                                     });
                                 }} />
                                 {/* Render state tab */}
-                                <If condition={tab ==="state" && ready} render={function () {
+                                <If condition={tab ==="state"} render={function () {
                                     return React.createElement(ConsoleStateTab, {
-                                        "files": Object.keys(self.viewer),
-                                        "getViewer": function (name) {
-                                            return self.viewer[name];
+                                        "getCurrentViewer": function () {
+                                            return self.getCurrentCell().viewer;
                                         },
                                         "onChange": self.handleStateChange
+                                    });
+                                }} />
+                                {/* Render schema tab */}
+                                <If condition={tab === "schema"} render={function () {
+                                    let schema = self.getCurrentCell().schema;
+                                    return React.createElement(ConsoleSchemaTab, {
+                                        "schema": JSON.stringify(schema, null, "    ")
                                     });
                                 }} />
                             </Console>
